e0254c56a0e181ac400545e336085fc801d98db0
candice2cc <pengcongjiayou@126.com>
2020-03-13 16:25:04 +0800

    增加测试环境配置。

--- library/src/main/java/com/finance/library/config/ServiceConstants.java
+++ library/src/main/java/com/finance/library/config/ServiceConstants.java
@@ -11,12 +11,12 @@ public class ServiceConstants {
 //    public static final String URL_BIND = "https://app-cas.noxgroup.com/api/third/binding";
 
     // 测试环境配置
-    public static final String URL_LOGIN = "http://10.8.7.243:8081/login/third";
-    public static final String URL_USER_INFO = "http://10.8.7.243:8081/api/user_info";
-    public static final String URL_REFRESH_TOKEN = "http://10.8.7.243:8081/oauth/token";
-    public static final String URL_LOGOUT = "http://10.8.7.243:8081/api/logout";
-    public static final String URL_SEND_TEL_CODE = "http://10.8.7.243:8081/api/login/sms";
-    public static final String URL_BIND = "http://10.8.7.243:8081/api/third/binding";
+    public static final String URL_LOGIN = "http://10.8.7.243:8100/login/third";
+    public static final String URL_USER_INFO = "http://10.8.7.243:8100/api/user_info";
+    public static final String URL_REFRESH_TOKEN = "http://10.8.7.243:8100/oauth/token";
+    public static final String URL_LOGOUT = "http://10.8.7.243:8100/api/logout";
+    public static final String URL_SEND_TEL_CODE = "http://10.8.7.243:8100/api/login/sms";
+    public static final String URL_BIND = "http://10.8.7.243:8100/api/third/binding";
 
     // 成功状态码
     public static final int CODE_OK = 0;
